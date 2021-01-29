6a5565de8273bfb556979e2e67bf134fddc0f23a
chaimjfishman <chaim.fishman2@gmail.com>
2021-01-29 18:26:12 -0500

    cleared default home screen

--- src/components/DefaultHome.tsx
+++ src/components/DefaultHome.tsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View } from 'react-native';
 import { AuthContext } from '../providers/AuthProvider';
-import { Contact } from '../constants/collectionTypes';
-import ForwardLink from '../components/ForwardLink';
 import { Button } from 'react-native-paper';
 
 
@@ -14,28 +12,15 @@ export default function DefaultHome(props: any) {
 
     const userID = user.id;
 
-    const contacts: Contact[] = []
-
     return (
         <View>
               <Button icon="ambulance" mode="contained" onPress={() => props.setCallMode(true)}>
                     Call mode
                 </Button>
 
-
             <Text>
                 Home Screen Here
             </Text>
-            <Text>
-                Contacts:
-            </Text>
-            <FlatList
-                data={contacts}
-                renderItem={Text}
-                renderItem={({item}) => <ForwardLink href={item.href} text={item.name}> </ForwardLink>}
-                // keyExtractor={(item) => item.id}
-                removeClippedSubviews={true}
-            />
         </View>
     );   
 }

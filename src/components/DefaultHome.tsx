1096c60cc4ce7629d68f1872a783346db4dfc3d6
chaimjfishman <chaim.fishman2@gmail.com>
2021-02-19 15:31:16 -0500

    undid Default Home test changes

--- src/components/DefaultHome.tsx
+++ src/components/DefaultHome.tsx
@@ -30,11 +30,8 @@ export default function DefaultHome(props: any) {
                 // const nextShift = await db.getNextShift(user.id);
                 // setShifts(nextShift)
                 console.log(nextShift);
-                // const monthlyHours = await db.getMonthlyHours(user.id);
-                // setMonthlyHours(monthlyHours)
-                const latestcall = await db.getLatestCall(user.id);
-                console.log('latestcll')
-                console.log(latestcall)
+                const monthlyHours = await db.getMonthlyHours(user.id);
+                setMonthlyHours(monthlyHours)
                 // const contacts = await db.getContacts();
                 // setContacts(contacts)
 

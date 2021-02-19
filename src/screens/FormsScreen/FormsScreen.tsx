150731df2ce2715c0ba3193387882cd724f4360e
Aliza Gindi <alizagindi@Alizas-MBP.attlocal.net>
2021-02-19 13:44:30 -0800

    small changes

--- src/screens/FormsScreen/FormsScreen.tsx
+++ src/screens/FormsScreen/FormsScreen.tsx
@@ -1,13 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import { SafeAreaView, View } from 'react-native';
-import styles from './styles';
 import FormLink from '../../components/FormLink';
-import LogoutBtn from '../../components/LogoutBtn'
 import { BottomTabScreenProps } from '../../constants/navigationScreenTypes';
 import Appbar from '../../components/Appbar';
 import * as storage from '../../utils/storage';
-
-import { ScrollView } from 'react-native-gesture-handler';
 export default function FormsScreen(props: BottomTabScreenProps<'Forms'>) {
     const [protocol, setProtocolform] = useState<String | null>(null);
 

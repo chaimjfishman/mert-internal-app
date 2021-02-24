4d9517e28e63141a311cf4c22de9ce318e1b12a9
PedroHSacramento <pedro.sacramento.oliveira@gmail.com>
2021-02-24 18:06:17 -0500

    Less space between form buttons

--- src/components/FormLink.tsx
+++ src/components/FormLink.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {  View, TouchableOpacity, Text, Platform} from 'react-native';
 import styles from './styles';
 import * as Linking from 'expo-linking';
-import { Card, Button } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button } from 'react-native-paper';
 
 type FormLinkProps = {
     title: string,
@@ -12,11 +10,9 @@ type FormLinkProps = {
 
 const FormLink = ({title, link}: FormLinkProps) => {
     return (
-        <SafeAreaView>
-                <Button style={[styles.callButton, {margin: 25, alignSelf: 'center'}]} mode="outlined" compact onPress={() => Linking.openURL(link)}>
-                    {title}
-                </Button>
-        </SafeAreaView>
+        <Button style={[styles.callButton, {margin: 5, alignSelf: 'center'}]} mode="outlined" compact onPress={() => Linking.openURL(link)}>
+            {title}
+        </Button>
     );   
 }
 

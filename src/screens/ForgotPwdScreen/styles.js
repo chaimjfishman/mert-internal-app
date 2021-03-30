aad08a6b4be4213eb412a106102ccdfce5f42b9b
Leah Voytovich <leahvoyt@gmail.com>
2021-03-30 02:52:09 -0400

    more password reset stuff

---
+++ src/screens/ForgotPwdScreen/styles.js
@@ -0,0 +1,67 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+
+    },
+    logo: {
+        flex: 1,
+        height: 120,
+        width: 120,
+        alignSelf: "center",
+        margin: 30
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16
+    },
+
+    button: {
+        backgroundColor: '#039BE5',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 5,
+        alignItems: "center",
+        textAlign: "center",
+        textAlignVertical: "middle",
+        justifyContent: 'center'
+    },
+
+    buttonTitle: {
+        color: 'red',
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    footerView: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 20
+    },
+    footerText: {
+        fontSize: 16,
+        color: '#2e2e2d'
+    },
+    footerLink: {
+        color: "#788eec",
+        fontWeight: "bold",
+        fontSize: 16
+    },
+    text: {
+        color: '#333',
+        fontSize: 24,
+        marginLeft: 25
+      },
+})

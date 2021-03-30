8fb1d676025daeda69fa0c3ce88eead469553f32
Leah Voytovich <leahvoyt@gmail.com>
2021-03-30 02:00:09 -0400

    initial ForgotPassword steps

---
+++ src/screens/ForgotPwdScreen/ForgotPwdScreen.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default function ForgotPassword(){
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Forgot Password Screen</Text>
+        </View>
+    )
+}
+
+

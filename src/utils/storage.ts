543dcc2f8fef882dcb7e078ed1ef456bf43c6db8
Sam May <samrmay@sas.upenn.edu>
2022-01-11 19:37:18 -0500

    Fix forms page not finding protocols

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,7 +2,7 @@ import { firebase } from './firebaseConfig';
 
 const storage = firebase.storage();
 
-const protocolRef = storage.refFromURL("gs://mert-internal-app.appspot.com/forms/2020 PA BLS Protocols.pdf");
+const protocolRef = storage.refFromURL("gs://mert-app-5ce19.appspot.com/2021 PA BLS Protocols FINAL 9-1-21.pdf");
 
 export async function getPAProtocol(): Promise<any>{
     const PAProtocol = await protocolRef.getMetadata();

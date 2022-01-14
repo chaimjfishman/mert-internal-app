703bc77c24bf33ca10dcdd8035df33dfeadf8bdd
Sam May <samrmay@sas.upenn.edu>
2022-01-14 00:05:11 -0500

    expose environment.js to expo

---
+++ environment.js
@@ -0,0 +1,22 @@
+export default function getEnvVars() {
+    return {
+
+        REACT_APP_APIKEY: "AIzaSyBOAIfltII9CPvVqtMKKdlZMZM7cma8HAg",
+      
+        REACT_APP_AUTHDOMAIN: "mert-app-5ce19.firebaseapp.com",
+      
+        databaseURL: "https://mert-app-5ce19.firebaseio.com",
+      
+        REACT_APP_PID: "mert-app-5ce19",
+      
+        REACT_APP_SB: "mert-app-5ce19.appspot.com",
+      
+        REACT_APP_SID: "933393895833",
+      
+        REACT_APP_APPID: "1:933393895833:web:59c5854bf4b1956f9d8308",
+
+        REACT_APP_MID: "G-V1KDTDZWYB"
+      
+      };
+      
+};
